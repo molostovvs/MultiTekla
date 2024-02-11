708b26ca8327c33a7b6bfbc3bda30eb0576ab4d6
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-02-11 18:57:22 +0300

    refactor: add fallback value

--- src/MultiTekla.Core/PluginManager.cs
+++ src/MultiTekla.Core/PluginManager.cs
@@ -19,7 +19,7 @@ public class PluginManager
     private IReadOnlyList<Assembly> LoadPluginAssemblies()
         => Directory.GetFiles(
                 Path.Combine(
-                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty,
                     PluginsFolderName
                 ),
                 "*.dll"
