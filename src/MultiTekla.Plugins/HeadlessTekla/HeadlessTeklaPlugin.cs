4933a8a052670dcc41782d071fc8fdd8df8f9b28
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-02-11 15:41:29 +0300

    update headless tekla plugin

--- src/MultiTekla.Plugins/HeadlessTekla/HeadlessTeklaPlugin.cs
+++ src/MultiTekla.Plugins/HeadlessTekla/HeadlessTeklaPlugin.cs
@@ -1,32 +1,59 @@
-using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
-using MultiTekla.Contracts;
+using MultiTekla.Plugins.Config;
+using MultiTekla.Plugins.Core;
 
 namespace MultiTekla.Plugins;
 
-public class HeadlessTeklaPlugin : IPlugin<object>
+public class HeadlessTeklaPlugin : IPlugin<TimeSpan>
 {
-    /*private HeadlessConfig? Config { get; set; }
-    
-    public HeadlessTeklaPlugin(HeadlessConfig config)
-        => Config = config;*/
+    public HeadlessConfig? Config { get; set; }
 
-    public object Run()
+    public TimeSpan Run()
     {
-        /*AppDomain.CurrentDomain.AssemblyResolve +=
+        if (Config is null)
+            throw new ArgumentNullException(
+                nameof(Config),
+                "You should provide config file when running headless tekla"
+            );
+
+        if (Config.TeklaBinPath is null || Config.EnvironmentIniPath is null
+            || Config.RoleIniPath is null)
+            throw new ArgumentException(
+                $"Config file is invalid, check {nameof(Config.TeklaBinPath)}, {
+                    nameof(Config.EnvironmentIniPath)}, {nameof(Config.RoleIniPath)}"
+            );
+
+        AppDomain.CurrentDomain.AssemblyResolve +=
             (_, a) => TeklaBinResolve(a, @"C:\TeklaStructures\2022.0\bin\");
 
+        var sw = new Stopwatch();
+        sw.Start();
+
         var headlessTs = Headless.BuildHeadless.With()
-           .BinDirectory(Config?.TeklaBinDirectory)
-           .ModelPath(Config?.ModelPath)
-           .EnvironmentPath(Config?.EnvironmentIniPath)
-           .RolePath(Config?.RoleIniPath)
+           .BinDirectory(Config.TeklaBinPath)
+           .EnvironmentPath(Config.EnvironmentIniPath)
+           .RolePath(Config.RoleIniPath)
            .Build();
 
-        headlessTs.Initialize(new DirectoryInfo(Config.ModelPath));*/
+        if (Config.ModelPath is null or "")
+            throw new ArgumentNullException(
+                nameof(Config.ModelPath),
+                $"{Config.ModelPath} should not be null or empty"
+            );
 
-        return new object();
+        if (!Directory.Exists(Config.ModelPath))
+            throw new ArgumentNullException(
+                nameof(Config.ModelPath),
+                $"Directory with path {nameof(Config.ModelPath)} doesn't exist"
+            );
+
+        headlessTs.Initialize(new DirectoryInfo(Config.ModelPath));
+
+        sw.Stop();
+
+        return sw.Elapsed;
     }
 
     private static Assembly? TeklaBinResolve(ResolveEventArgs args, string tsBinDirectory)
