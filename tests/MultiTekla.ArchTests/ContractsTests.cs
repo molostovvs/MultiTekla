bb0da859e62ce80181b229134f81c3b71242fa0f
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-03-31 16:57:28 +0300

    tests:add contracts tests

---
+++ tests/MultiTekla.ArchTests/ContractsTests.cs
@@ -0,0 +1,38 @@
+﻿using ArchUnitNET.Domain;
+using ArchUnitNET.Loader;
+using ArchUnitNET.NUnit;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+
+namespace MultiTekla.ArchTests;
+
+public class ContractsTests
+{
+    private static readonly Architecture Architecture = new ArchLoader()
+       .LoadAssemblies(
+            System.Reflection.Assembly.Load("MultiTekla.CLI"),
+            System.Reflection.Assembly.Load("MultiTekla.Contracts"),
+            System.Reflection.Assembly.Load("MultiTekla.Core"),
+            System.Reflection.Assembly.Load("MultiTekla.Plugins")
+        )
+       .Build();
+
+    private readonly IObjectProvider<IType> _contractsAssembly =
+        Types().That().ResideInAssembly(typeof(Contracts.PluginBase).Assembly).As("Contracts");
+
+    [Test]
+    public void Test()
+    {
+        var result = PropertyMembers()
+            .That()
+            .HavePublicGetter()
+            .And()
+            .AreDeclaredIn(_contractsAssembly)
+            .And()
+            .HaveNameEndingWith("Plugin")
+            .Should()
+            .HaveAttributeWithArguments(typeof(System.Composition.ImportMetadataConstraintAttribute),
+                                        new object[] { "name", });
+
+        result.Check(Architecture);
+    }
+}
