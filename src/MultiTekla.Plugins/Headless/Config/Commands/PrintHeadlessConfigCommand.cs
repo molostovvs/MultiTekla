66284eaaf4ead81fcb762b55a7d58d65b382fbb0
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-04-01 09:09:17 +0300

    feat:hide options in print config plugin

--- src/MultiTekla.Plugins/Headless/Config/Commands/PrintHeadlessConfigCommand.cs
+++ src/MultiTekla.Plugins/Headless/Config/Commands/PrintHeadlessConfigCommand.cs
@@ -3,6 +3,9 @@
 [Command("headless config print", Description = "Prints headless config")]
 public class PrintHeadlessConfigCommand : CommandBase<GetHeadlessConfigPlugin>
 {
+    [CommandParameter(0, Name = "CONFIG NAME", Description = "Config name to print")]
+    public new required string? ConfigName { get; init; }
+
     protected override ValueTask Execute(IConsole console, GetHeadlessConfigPlugin plugin)
     {
         plugin.Config = new HeadlessConfig { Name = ConfigName, };
@@ -11,4 +14,10 @@ public class PrintHeadlessConfigCommand : CommandBase<GetHeadlessConfigPlugin>
 
         return default;
     }
+
+    [Obsolete("This property is meaningless in this command", true)]
+    public new string? ModelName { get; init; }
+
+    [Obsolete("This property is meaningless in this command", true)]
+    public new bool IsHeadlessMode { get; init; }
 }
