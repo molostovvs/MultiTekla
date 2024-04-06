0607d8d07b032e25f25d9a4cd439f8622a9ac160
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-04-06 17:49:39 +0300

    chore:update command description

--- src/MultiTekla.Plugins/Headless/Config/Commands/RootHeadlessConfigCommand.cs
+++ src/MultiTekla.Plugins/Headless/Config/Commands/RootHeadlessConfigCommand.cs
@@ -1,6 +1,6 @@
 ﻿namespace MultiTekla.Plugins.Headless.Config.Commands;
 
-[Command("headless config", Description = "Manage config file for headless tekla")]
+[Command("headless config", Description = "Manage config file for headless Tekla")]
 public class RootHeadlessConfigCommand : CommandBase<CreateHeadlessConfigPlugin>
 {
     protected override ValueTask Execute(IConsole console, CreateHeadlessConfigPlugin plugin)
