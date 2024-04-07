1c0dd3ecfd8ff97554d74f44ae298bde9aa71435
Victor Molostov <86685532+ConcreteEcho@users.noreply.github.com>
2024-04-07 16:30:45 +0300

    feat:add ifc2x3 export plugin
    just an MVP, with only one option at the moment

---
+++ src/plugins/IfcExport/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Threading.Tasks;
+global using CliFx.Attributes;
+global using CliFx.Infrastructure;
+global using MultiTekla.Contracts;
+global using TSG = Tekla.Structures.Geometry3d;
+global using TSM = Tekla.Structures.Model;
---
+++ src/plugins/IfcExport/Ifc2x3ExportCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace IfcExport;
+
+[Command("export ifc2x3", Description = "Export model to IFC2x3")]
+public sealed class Ifc2x3ExportCommand : CommandBase<Ifc2x3ExportPlugin>
+{
+    [CommandParameter(0, Name = "MODEL NAME", Description = "The name of the model to be exported to IFC", IsRequired = false)]
+    public override string? ModelName { get; init; }
+
+    [CommandOption("output", 'o', Description = "Path to file for output")]
+    public string? OutputFile { get; init; }
+
+    [CommandOption("config", 'c', Description = "Config to use for headless run")]
+    public override string ConfigName { get; init; } = "default";
+
+    [CommandOption("headless", 'l', Description = "Run plugin with headless Tekla")]
+    public override bool IsHeadlessMode { get; init; } = true;
+
+    protected override ValueTask Execute(IConsole console, Ifc2x3ExportPlugin plugin)
+    {
+        plugin.OutputFile = OutputFile;
+        plugin.RunPlugin();
+        return default;
+    }
+}
